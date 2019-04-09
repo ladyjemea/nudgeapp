16980ca1c39e732e71905ac3b807939943cb11fe
Craciun Cosmin-Radu <cosmin.radu.craciun@gmail.com>
2019-04-09 12:16:31 +0200

    Stopped the service

--- NudgeApp.DataAnalysis/ScheduledServices/SpareTimeNudgeTask.cs
+++ NudgeApp.DataAnalysis/ScheduledServices/SpareTimeNudgeTask.cs
@@ -13,11 +13,11 @@
     {
         public SpareTimeNudgeTask(IServiceScopeFactory serviceScopeFactory) : base(serviceScopeFactory) { }
 
-        protected override string Schedule => "*/10 * * * * *";
+        protected override string Schedule => " * */1 * * * *";
 
         public override async Task ProcessInScope(IServiceProvider serviceProvider)
         {
-            var weatherService = serviceProvider.GetService<IWeatherService>();
+            /*var weatherService = serviceProvider.GetService<IWeatherService>();
             var forecast = await weatherService.GetCurrentForecast();
 
             if (forecast.WeatherCondition == WeatherCondition.StrongWinds)
@@ -44,7 +44,7 @@
                 {
                     pushNotificationService.PushToUser(userId, "Nudge of the day", "It's Rainy");
                 }
-            }
+            }*/
         }
     }
 }
